161c19abedd8b7057e03fa3f204b7fa555d1ca3f
Daniel Schmith <schmithd@cylex.ro>
2017-04-25 11:40:42 +0300

    changes

--- lib/Crawler.php
+++ lib/Crawler.php
@@ -4,6 +4,8 @@ namespace Cylex\Crawlers\Facebook;
 use Exception;
 use Facebook\Facebook;
 use Cylex\Crawlers\Facebook\ErrorHandler;
+use Facebook\Authentication\AccessToken;
+
 
 class Crawler
 {    
@@ -28,8 +30,7 @@ class Crawler
         $this->sleepTime = $sleepTime;
         $this->processed = $processed;
         $this->chunk = $chunk;
-    }
-    
+    }    
     
     public function run(Facebook $app, array $fields)
     {   
@@ -44,59 +45,59 @@ class Crawler
             $batch = array();
             
             $data = $this->source->getNext();
-           // print_r($data);
-            //sleep(2);
             
-//            if(!isset($data['data'])) 
-//            {
-//                if(isset($data['allfirnr']))
-//                {
-//                    $this->source->updateProcessed($data['allfirnr'], 1);
-//                }
-//                
-//                break;
-//            }
-//            
-//            foreach($data['data'] as $firnr => &$fb)
-//            {
-//                $batch[$firnr] = $app->request('GET', '/' . $fb['id'] . '?fields=' . $queryFields . '&locale=' . $fb['locale']);
-//            }
-//            
-//            try
-//            {
-//                $responses = $app->sendBatchRequest($batch);
-//            }
-//            catch(Exception $e)
-//            {
-//                continue;
-//            }
-//            
-//            foreach($responses as $firnr => $response)
-//            {
-//                $body = null;
-//                $data['data'][$firnr]['statusCode'] = 0;
-//                $body = $response->getBody();
-//                
-//                if($response->isError())
-//                {
-//                    $body = json_decode($body, true);
-//                    $errorHandler = new ErrorHandler($firnr, $data['data'][$firnr], $body['error'], $this->source);
-//                    $body = null;
-//                    
-//                    $data['data'][$firnr]['statusCode'] = $errorHandler->getStatusCode();
-//                    
-//                    if($errorHandler->handleError())
-//                    {                        
-//                       unset($data['allfirnr'][$firnr]);
-//                    }
-//                }               
-//                
-//                $this->target->save($data['data'][$firnr]['crw_fir_nr'], $data['data'][$firnr]['id'], $data['data'][$firnr]['locale'], $body, $data['countryCode'], $data['sid'], $data['data'][$firnr]['statusCode']);
-//                
-//            }
-            print_r($data['allfirnr']);die;
-            $this->source->updateProcessed($data['allfirnr'], 1);
-            echo "update";die;
+            if(!isset($data['data'])) 
+            {
+                if(isset($data['allfirnr']))
+                {
+                    $this->source->updateProcessed($data['allfirnr'], 1);
+                }
+                
+                break;
+            }
+
+            foreach($data['data'] as $firnr => &$fb)
+            {
+                
+                $batch[$firnr] = $app->request('GET', '/' . $fb['id']. '?fields=' . $queryFields . '&locale=' . $fb['locale']);
+            }
+            
+            try
+            {
+                $responses = $app->sendBatchRequest($batch);
+            }
+            catch(Exception $e)
+            {
+                print_r($e);
+                continue;
+            }
+            
+            foreach($responses as $firnr => $response)
+            {
+                $body = null;
+                $data['data'][$firnr]['statusCode'] = 0;
+                $body = $response->getBody();
+                
+                if($response->isError())
+                {
+                    $body = json_decode($body, true);
+                    print_r($body);
+                    $errorHandler = new ErrorHandler($firnr, $data['data'][$firnr], $body['error'], $this->source);
+                    $body = null;
+                    
+                    $data['data'][$firnr]['statusCode'] = $errorHandler->getStatusCode();
+                    
+                    if($errorHandler->handleError())
+                    {                        
+                       unset($data['allfirnr'][$firnr]);
+                    }
+                }               
+                
+                $this->target->save($data['data'][$firnr]['crw_fir_nr'], $data['data'][$firnr]['id'], $data['data'][$firnr]['locale'], $body, $data['countryCode'], $data['sid'], $data['data'][$firnr]['statusCode']);
+                die;
+            }
+            
+            $this->source->updateProcessed($data['allfirnr'], 1);            
             sleep($this->sleepTime);
         }
         
